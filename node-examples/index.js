98739a8fd81ffa1e927bd1b8b336570d7bde381c
islam36 <jm_boumendjel@esi.dz>
2020-10-04 08:27:24 +0100

    node callbacks

--- node-examples/index.js
+++ node-examples/index.js
@@ -1,3 +1,4 @@
+const rectangle = require('./rectangle');
 var rect = require('./rectangle');
 
 
@@ -5,15 +6,20 @@ var rect = require('./rectangle');
 function solveRec(l,b){
     console.log('solving for rectangle with l=' + l + ' and b=' + b + ':');
     
-    if (l <= 0 || b <= 0){
-        console.log('rectangle dimensions should be > 0');
-    }
-    else{
-        console.log('area = '+ rect.area(l,b));
-        console.log('permeter = ' + rect.perimeter(l,b));
-    }
+    rect(l,b, (err, rectangle) => {
+        if(err){
+            console.log("ERROR:" + err.message);
+        }
+        else{
+            console.log("area = " + rectangle.area() );
+            console.log("perimeter = " + rectangle.perimeter() );
+        }
+    });
+
+    console.log("this line is after calling the rect() ");
 }
 
 
 solveRec(3,4);
+console.log("\n");
 solveRec(0,5);
